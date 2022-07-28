d18b3467a9483cc446d41056e7ea0201059db982
Nóra Gyopár Muraközy <s202222@dtu.dk>
2022-07-28 14:03:37 +0200

    Create and style home page (Main)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Main from '../components/Main'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar/>
+      <Main/>
     </div>
   )
 }
