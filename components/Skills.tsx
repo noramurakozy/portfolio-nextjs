f213b29582dba51ffc26688c827f98b3180640ce
Nóra Gyopár Muraközy <s202222@dtu.dk>
2022-07-28 15:57:22 +0200

    Extract style of sub-header, change title

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -6,7 +6,7 @@ const Skills = () => {
   return (
     <div className='w-full lg:h-screen p-2'>
         <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
-            <p className='text-xl tracking-widest uppercase text-blue-purple'>Skills</p>
+            <p className='sub-header'>Skills</p>
             <h2 className='py-4'>What Can I Do</h2>
             <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
                 <SkillCard imgPath='/../public/assets/skills/html.png' imgAlt='Html logo' label='HTML'/>
