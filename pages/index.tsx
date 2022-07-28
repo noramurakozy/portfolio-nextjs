c08b29c9fa6cc14bd07d36a9cbae559874c13e6f
Nóra Gyopár Muraközy <s202222@dtu.dk>
2022-07-28 14:50:36 +0200

    Create and style the About section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import About from '../components/About'
 import Main from '../components/Main'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
@@ -15,6 +16,7 @@ const Home: NextPage = () => {
       </Head>
       <Navbar/>
       <Main/>
+      <About/>
     </div>
   )
 }
